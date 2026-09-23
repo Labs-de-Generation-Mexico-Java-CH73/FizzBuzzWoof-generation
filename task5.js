6b36149355c70167b04524dd36698a10bb5774c1
Carlos Ignacio Estrada Chairez <carlosestrchz@gmail.com>
2026-09-23 16:50:52 -0600

    develop: se completa el task 5

--- task5.js
+++ task5.js
@@ -1,5 +1,12 @@
 // Refer to Task 5 in your Instructions to complete this task
 
-for (let i = 0; i < 1; i++) {
-    console.log("This is Task Five!");
-  };
+const prompt = require("prompt-sync")();
+const limit = parseInt(prompt("¿Cuántas líneas deseas generar? "));
+
+for (let i = 1; i <= limit; i++) {
+  let output = "";
+  
+  if (i % 3 === 0) output += "Fizz";
+  if (i % 5 === 0) output += "Buzz";
+  if (i % 7 === 0) output += "Woof";
+}
