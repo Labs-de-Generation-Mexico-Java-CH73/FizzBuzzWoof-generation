728bed1ea620568b80ef28a962409fab47b29e4c
Carlos Ignacio Estrada Chairez <carlosestrchz@gmail.com>
2026-09-23 17:06:31 -0600

    develop: se desarrolla el task 6

--- task6.js
+++ task6.js
@@ -1,5 +1,18 @@
 // Refer to Task 6 in your Instructions to complete this task
 
-for (let i = 0; i < 1; i++) {
-    console.log("This is Task Six!");
-  };
+const limit = parseInt(prompt("¿Cuántas líneas deseas generar? "));
+const resultados = [];
+
+for (let i = 1; i <= limit; i++) {
+  let output = "";
+  
+  if (i % 3 === 0) output += "Fizz";
+  if (i % 5 === 0) output += "Buzz";
+  if (i % 7 === 0) output += "Woof";
+
+  //se asigna el resultado al arreglo
+  resultados.push(output || i);
+}
+
+// imprimo el arreglo completo una vez que el ciclo se acaba
+console.log(resultados);
